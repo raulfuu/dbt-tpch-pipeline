71d4ef4ea7212a445d7b80f81e70a72fed5c2223
raulfuente <raul.fuente@sdggroup.com>
2026-06-08 12:23:57 +0200

    feat: implement segment_managers seed file to map static business targets to dim_customers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,17 +6,25 @@ with customers_with_geo as (
     select * from {{ ref('int_customers_with_geo') }}
 ),
 
+segment_managers as (
+    select * from {{ ref('segment_managers') }}
+),
+
 final as (
     select
-        customer_id,
-        customer_name,
-        customer_address,
-        phone_number,
-        account_balance,
-        market_segment,
-        nation_name,
-        region_name
-    from customers_with_geo
+        c.customer_id,
+        c.customer_name,
+        c.customer_address,
+        c.phone_number,
+        c.account_balance,
+        c.market_segment,
+        c.nation_name,
+        c.region_name,
+        sm.segment_manager,
+        sm.profit_target
+    from customers_with_geo c
+    left join segment_managers sm
+        on c.market_segment = sm.market_segment
 )
 
 select * from final
